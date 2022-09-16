a6a53915d6469316e40ce3ee44685095ebef400a
Bruno Sanches <brunosanches1@outlook.com>
2022-09-16 08:40:42 +0200

    Finish tp1

--- Seance_1/TP_1/reponses.md
+++ Seance_1/TP_1/reponses.md
@@ -1,16 +1,96 @@
 ## Question 1
+Pour faire sont attaque, le lycanthrope fouille son environnement
+pour les villageois. Selon la définition d'agent vue en cours, 
+un agent est capable de percevoir son environnement et d'agir dessus.
+M'implémentation définit un agent, le lycanthrope, capable de percevoir son
+environnement et des autres agents présents, les villageois, 
+et d'agir (attaquer les villageois).
 
 ## Question 2
+Les lycanthropes ont gaigné dans presque toutes les simulations faites, cela veut dire,
+les lycanthropes on transformé tous les villageois en lycanthropes. 
+La système prendre souvent plus de 500 cycles pour converger vers un résultat.
+
+Le changement dans le nombre d'apothicaires ne fait pas assez de différence, 
+vue qu'un apothicaire doit être très proche d'un lycanthrope et celui ne doit pas être
+transformée, la majorité des villageois infectés se transforment avant qu'un apothicaire
+soit proche. L'augmentation du nombre de chasseurs peut entraîner le système à un résultat
+ou soit tous les lycanthropes sont tués, soit il n'y a pas de convergence parce que 
+les lycanthropes n'arrivent pas à attaquer tous les villageois, et ni les chasseurs
+sont assez proches des lycanthropes pour les tuer. Si on augmente le nombre de
+lycanthropes au début de la simulation, le système peut converger plus rapidement.
 
 ## Question 3
+![](images/graphique_ex3.png)
+
+Il est possible de regarder sur le graphique qu'après quelques cycles, 
+presque tous les villageois ont été transformés en lycanthropes,
+tous les villageois ont été transformés en lycanthropes après 700 cycles.
 
 ## Question 4
+La simulation avec un nombre plus grande d'apothicaires(10) a généré le graphique suivant.
+![](images/graphique_ex3_apothicaires.png)
+
+Dans ce graphique on peut voit qu'augmenter le nombre d'apothicaires n'a pas fait 
+un effet sur la convergence du système, les lycanthropes ont transformé tous les 
+villageois avant le fin de la simulation.
+
+a simulation avec un nombre plus grande de chasseurs(10) a généré le graphique suivant.
+![](images/graphique_ex3_chasseurs.png)
+
+Dans cette simulation, les lycanthropes ont été tous tués par les chasseurs pendant
+qu'il a resté des humains non-lycanthropes.
+
+Ces graphiques correspondent à l'idée que l'apothicaire ne fait pas assez de 
+différence dans le système, et que si on augmente le nombre de chasseurs, tous
+les lycanthropes seront tués.
 
 ## Question 5
+Les paramètres qui ont plus d'influence sur la simulation sont le nombre 
+de lycanthropes et de chasseurs, le nombre de lycanthropes peut définir si la 
+simulation convergera à un résultat ou les lycanthropes ont gagné, ou ils ont perdu parce
+qu'ils sont ou tués par les chasseurs ou n'ont pat attaqué tous les villageois. 
+Le nombre de chasseurs influence la probabilité d'un lycanthrope de trouver
+un chasseur et d'être tué, s'on l'augmente, les lycanthropes seront tous tués.
 
 ## Question 6
+Le nombre d'apothicaires peut faire que la simulation soit terminé avec quelques 
+villageois restants, mais dans la majorité des cas, les lycanthropes infecterons
+tous les villageois. 
 
 ## Question 7
+|    |   n_apothicaires |   Run |   Humains |   Lycanthropes |   Lycanthropes transformés |   Total |   n_villagers |   n_lycanthropes |   n_chasseurs |
+|---:|-----------------:|------:|----------:|---------------:|---------------------------:|--------:|--------------:|-----------------:|--------------:|
+|  0 |                0 |     0 |         0 |             35 |                         35 |      36 |            50 |                5 |             1 |
+|  1 |                1 |     1 |         0 |             34 |                         34 |      36 |            50 |                5 |             1 |
+|  2 |                2 |     2 |         0 |             38 |                         38 |      41 |            50 |                5 |             1 |
+|  3 |                3 |     3 |         0 |             30 |                         30 |      34 |            50 |                5 |             1 |
+|  4 |                4 |     4 |         0 |             35 |                         35 |      40 |            50 |                5 |             1 |
+|  5 |                5 |     5 |         0 |             43 |                         43 |      49 |            50 |                5 |             1 |
+
+Les résultats sont d'accord avec l'hypotheses, dans tous les cas les lycanthropes ont infectés tous les villageois.
+On peut conclure à partir de ces simulations que le résultat de la simulation ne dépend pas de le nombre d'apothicaires.
 
 ## Question bonus
+Le temps pour essayer tous les combinations de paramétrés croisse avec le nombre de paramétrés et leur domain,
+chaque paramétré ajouté multiple le temps de simulation par le nombre de valeurs possibles, pour faire varier tous les
+variables, la simulation peut prendre un temps prohibitif.
+
+La solution est d'utiliser la fonction batch_run(), le paramétrè number_processes, quand définit comme *None* utilise
+tous les processeurs disponibles du ordinateur, en tournant les simulations en paraléle.
+
+```
+def run_batchMP():
+    result = batch_run(Village,
+              parameters={
+                  "n_apothicaires": range(0, 6, 1),
+                  "n_villagers": range(25, 50, 5),
+                  "n_lycanthropes": range(2, 10, 2),
+                  "n_chasseurs": range(0, 6, 1)
+              },
+              number_processes=None)
+
+    print(result)
+```
+
 
--- Seance_1/TP_1/village.py
+++ Seance_1/TP_1/village.py
@@ -6,23 +6,56 @@ from collections import defaultdict
 import mesa
 import tornado, tornado.ioloop
 from mesa import space
+from mesa.batchrunner import BatchRunner, BatchRunnerMP, batch_run
 from mesa.time import RandomActivation
-from mesa.visualization.ModularVisualization import ModularServer, VisualizationElement
-
+from mesa.visualization.ModularVisualization import ModularServer, VisualizationElement, UserSettableParameter
+from mesa.visualization.modules import ChartModule
 
 class Village(mesa.Model):
 
-    def __init__(self, n_villagers):
+    def __init__(self, n_villagers, n_lycanthropes, n_apothicaires, n_chasseurs):
         mesa.Model.__init__(self)
         self.space = mesa.space.ContinuousSpace(600, 600, False)
         self.schedule = RandomActivation(self)
+        self.datacollector = mesa.DataCollector({
+            "Humains": lambda m: len([personne for personne in m.schedule.agents
+                                      if type(personne) == Villager and
+                                      not personne.est_lycanthrope]),
+
+            "Lycanthropes": lambda m: len([personne for personne in m.schedule.agents
+                                      if type(personne) == Villager and
+                                      personne.est_lycanthrope]),
+
+            "Lycanthropes transformés": lambda m: len([personne for personne in m.schedule.agents
+                                      if type(personne) == Villager and
+                                      personne.est_lycanthrope and
+                                      personne.est_transforme]),
+
+            "Total": lambda m: m.schedule.get_agent_count()
+        })
+
         for _ in range(n_villagers):
-            self.schedule.add(Villager(random.random() * 500, random.random() * 500, 10, int(uuid.uuid1()), self))
+            self.schedule.add(Villager(random.random() * 500, random.random() * 500, 10, int(uuid.uuid1()), self, 
+                                       False))
+
+        for _ in range(n_lycanthropes):
+            self.schedule.add(Villager(random.random() * 500, random.random() * 500, 10, int(uuid.uuid1()), self,
+                                       True))
+
+        for _ in range(n_apothicaires):
+            self.schedule.add(Cleric(random.random() * 500, random.random() * 500, 10, int(uuid.uuid1()), self))
+
+        for _ in range(n_chasseurs):
+            self.schedule.add(Hunter(random.random() * 500, random.random() * 500, 10, int(uuid.uuid1()), self))
 
     def step(self):
         self.schedule.step()
+        self.datacollector.collect(self)
+
         if self.schedule.steps >= 1000:
             self.running = False
+
+
 
 
 class ContinuousCanvas(VisualizationElement):
@@ -65,8 +98,49 @@ def wander(x, y, speed, model):
 
     return new_x, new_y
 
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
 
 class Villager(mesa.Agent):
+    def __init__(self, x, y, speed, unique_id: int, model: Village, est_lycanthrope: bool):
+        super().__init__(unique_id, model)
+        self.pos = (x, y)
+        self.speed = speed
+        self.model = model
+        self.est_lycanthrope = est_lycanthrope # Paramètre indicant si le villager est un lycanthrope
+        self.est_transforme = False # Les villagers qui sont lycanthropes commences sans êtres transformés
+
+    def portrayal_method(self):
+        color = "red" if self.est_lycanthrope else "blue" # Lycanthropes sont affichés en rouge
+        r = 6 if self.est_lycanthrope and self.est_transforme else 3 # Lycanthropes transformers ont une taille plus grande
+        portrayal = {"Shape": "circle",
+                     "Filled": "true",
+                     "Layer": 1,
+                     "Color": color,
+                     "r": r}
+        return portrayal
+
+    def step(self):
+        self.pos = wander(self.pos[0], self.pos[1], self.speed, self.model)
+
+        if self.est_lycanthrope:
+            if random.random() < 0.1: # 10% de chance d'un lycanthrope se transformer
+                self.est_transforme = True
+
+            if self.est_transforme:
+                # Cherche des villager proches
+                villager_proches = [personne for personne in self.model.schedule.agents
+                                        if type(personne) == Villager and
+                                        not personne.est_lycanthrope and
+                                        distance(self.pos[0], self.pos[1],
+                                                 personne.pos[0], personne.pos[1]) < 40.0]
+
+                # Fait l'attaque
+                for villager in villager_proches:
+                    villager.est_lycanthrope = True
+
+class Cleric(mesa.Agent):
     def __init__(self, x, y, speed, unique_id: int, model: Village):
         super().__init__(unique_id, model)
         self.pos = (x, y)
@@ -74,7 +148,7 @@ class Villager(mesa.Agent):
         self.model = model
 
     def portrayal_method(self):
-        color = "blue"
+        color = "green" # Les apothicaires sont affiches en verte
         r = 3
         portrayal = {"Shape": "circle",
                      "Filled": "true",
@@ -86,16 +160,114 @@ class Villager(mesa.Agent):
     def step(self):
         self.pos = wander(self.pos[0], self.pos[1], self.speed, self.model)
 
+        # Cherche des villager proches qui sont des lycanthropes non tranformées
+        lycanthrope_proches = [personne for personne in self.model.schedule.agents
+                                if type(personne) == Villager and
+                                personne.est_lycanthrope and
+                                not personne.est_transforme and
+                                distance(self.pos[0], self.pos[1],
+                                         personne.pos[0], personne.pos[1]) < 30.0]
+
+        # rechange un lycanthrope en humain
+        for lycanthrope in lycanthrope_proches:
+            lycanthrope.est_lycanthrope = False
+
+class Hunter(mesa.Agent):
+    def __init__(self, x, y, speed, unique_id: int, model: Village):
+        super().__init__(unique_id, model)
+        self.pos = (x, y)
+        self.speed = speed
+        self.model = model
+
+    def portrayal_method(self):
+        color = "black" # Les apothicaires sont affiches en verte
+        r = 3
+        portrayal = {"Shape": "circle",
+                     "Filled": "true",
+                     "Layer": 1,
+                     "Color": color,
+                     "r": r}
+        return portrayal
+
+    def step(self):
+        self.pos = wander(self.pos[0], self.pos[1], self.speed, self.model)
+
+        # Cherche des lycanthropes transformes proches
+        lycanthrope_proches = [personne for personne in self.model.schedule.agents
+                                if type(personne) == Villager and
+                                personne.est_lycanthrope and
+                                personne.est_transforme and
+                                distance(self.pos[0], self.pos[1],
+                                         personne.pos[0], personne.pos[1]) < 40.0]
+
+        # tue le lycanthrope
+        for lycanthrope in lycanthrope_proches:
+            self.model.schedule.remove(lycanthrope)
 
 def run_single_server():
     server = ModularServer(Village,
-                           [ContinuousCanvas()],
+                           [ContinuousCanvas(), ChartModule([
+                               { "Label": "Lycanthropes", "Color": "Blue"},
+                               { "Label": "Lycanthropes transformés", "Color": "Red"},
+                               { "Label": "Humains", "Color": "Yellow"},
+                               { "Label": "Total", "Color": "Green"}
+                            ], data_collector_name="datacollector")],
                            "Village",
-                           {"n_villagers": 25})
+                           {"n_villagers": UserSettableParameter('slider', 'Villageois sains', 25, 1, 50, 1),
+                            "n_lycanthropes": UserSettableParameter('slider', 'Lycanthropes', 5, 1, 50, 1),
+                            "n_apothicaires": UserSettableParameter('slider', 'Apothicaires', 1, 0, 50, 1),
+                            "n_chasseurs": UserSettableParameter('slider', 'Chasseurs', 2, 0, 50, 1)})
     server.port = 8521
     server.launch()
     tornado.ioloop.IOLoop.current().stop()
 
+def run_batch():
+    batch = BatchRunner(Village,
+                        variable_parameters={
+                            "n_apothicaires": range(0, 6, 1)
+                        },
+                        fixed_parameters={
+                            "n_villagers": 50,
+                            "n_lycanthropes": 5,
+                            "n_chasseurs": 1,
+                        },
+                        model_reporters={
+                            "Humains": lambda m: len([personne for personne in m.schedule.agents
+                                                      if type(personne) == Villager and
+                                                      not personne.est_lycanthrope]),
+
+                            "Lycanthropes": lambda m: len([personne for personne in m.schedule.agents
+                                                           if type(personne) == Villager and
+                                                           personne.est_lycanthrope]),
+
+                            "Lycanthropes transformés": lambda m: len([personne for personne in m.schedule.agents
+                                                                       if type(personne) == Villager and
+                                                                       personne.est_lycanthrope and
+                                                                       personne.est_transforme]),
+
+                            "Total":  lambda m: m.schedule.get_agent_count()
+                        })
+
+    batch.run_all()
+    df = batch.get_model_vars_dataframe()
+    print(df.to_markdown())
+
+def run_batchMP():
+    result = batch_run(Village,
+              parameters={
+                  "n_apothicaires": range(0, 6, 1),
+                  "n_villagers": range(25, 50, 5),
+                  "n_lycanthropes": range(2, 10, 2),
+                  "n_chasseurs": range(0, 6, 1)
+              },
+              number_processes=None)
+
+    print(result)
+    #batch.run_all()
+    #df = batch.get_model_vars_dataframe()
+    #print(df.to_markdown())
 
 if __name__ == "__main__":
-    run_single_server()
+    #run_single_server()
+    #run_batch()
+    run_batchMP()
